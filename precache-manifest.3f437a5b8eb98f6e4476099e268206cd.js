397cdafa60980f0d409852d095006ab6125f2af4
Wagle Brito <waglebr@gmail.com>
2019-01-08 13:08:43 -0800

    Updates

---
+++ precache-manifest.3f437a5b8eb98f6e4476099e268206cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d730c952258bfc163ba1",
+    "url": "/waglebr/reactjs/static/js/runtime~main.d730c952.js"
+  },
+  {
+    "revision": "ab333549162a92dd3a42",
+    "url": "/waglebr/reactjs/static/js/main.ab333549.chunk.js"
+  },
+  {
+    "revision": "41dbcc49625af11e470a",
+    "url": "/waglebr/reactjs/static/js/1.41dbcc49.chunk.js"
+  },
+  {
+    "revision": "ab333549162a92dd3a42",
+    "url": "/waglebr/reactjs/static/css/main.fb0345f2.chunk.css"
+  },
+  {
+    "revision": "41dbcc49625af11e470a",
+    "url": "/waglebr/reactjs/static/css/1.7989c8f9.chunk.css"
+  },
+  {
+    "revision": "b73cba5c1f4037c69c612d60897b4116",
+    "url": "/waglebr/reactjs/index.html"
+  }
+];
